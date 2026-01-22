bdd9bde3944fa1dc949c2a3c362f4a42ea616d29
AndreMauro14 <andremauro87@gmail.com>
2026-01-22 18:08:47 -0300

    feat: agregação de estatísticas por jogador (Gols, xG, etc)

--- DFbr_2024.py
+++ DFbr_2024.py
@@ -18,4 +18,18 @@ df_dadosFut = pd.read_csv("dados_transfermarkt_2024.csv")
 df_kaggle["nome_join"] = df_kaggle["Jogador"].str.lower().str.strip()
 df_kaggle['Idade'] = df_kaggle['Idade'].apply(lambda x: str(x).split('-')[0])
 df_dadosFut["nome_join"] = df_dadosFut["Name"].str.lower().str.strip()
-df_precos_limpo = df_dadosFut.drop_duplicates(subset=['nome_join'], keep='first')        
+df_precos_limpo = df_dadosFut.drop_duplicates(subset=['nome_join'], keep='first')
+
+cols_numericas = ['Gols','xG','Assis.','xAG','Bloqueios']
+for col in cols_numericas:
+    df_kaggle[col] = pd.to_numeric(df_kaggle[col], errors='coerce').fillna(0)
+        
+df_stats_limpo = df_kaggle.groupby('nome_join').agg({
+    'Gols': 'sum',
+    'xG': 'sum',
+    'Assis.': 'sum',
+    'xAG': 'sum',
+    'Bloqueios': 'sum',
+    'Time': 'first',
+    'Idade': 'first'
+}).reset_index()      
